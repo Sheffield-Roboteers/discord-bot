05e99dd274b616ecf50e184bfc0f343ec40116eb
Sam Piper <sampii@protonmail.com>
2022-11-10 23:38:45 +0000

    feat: added slash commands and updated docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
-FROM alpine:latest
+FROM node:16-alpine
 
 WORKDIR /usr/src/bot
 COPY package.json config.json ./
 
-RUN apk add --update \
-    && apk add --no-cache nodejs-current npm \
-    && apk add --no-cache --virtual .build git curl build-base g++ \
-    && npm install \
-    && apk del .build
+RUN npm install 
 
 COPY . .
 
--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 // Required requirements and constants
-const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton} = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton, Collection} = require('discord.js');
 const config = require('./config.json');
 const myIntents = new Intents(32767);
 
 // Create new client instance
 const client = new Client({ intents: myIntents , partials:['MESSAGE', 'CHANNEL', 'REACTION']});
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 // Client Events
 client.on('ready', c => onReady(c));
@@ -20,6 +38,7 @@ client.on("error", (error) => console.log(error));
  */
  async function onReady(c) {
     console.log(`Ready! Logged in as ${c.user.tag}`);
+    client.user.setActivity(`/help | ${client.user.username}`, { type: "PLAYING" })
     const channel = client.channels.cache.find((channel) => channel.id === config.roleChannel);
     // channel will not contain messages after it is found
     try {
@@ -42,6 +61,23 @@ client.on("error", (error) => console.log(error));
         console.log(`Message not found`);
     }
   }
+  client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    const command = interaction.client.commands.get(interaction.commandName);
+  
+    if (!command) {
+      console.error(`No command matching ${interaction.commandName} was found.`);
+      return;
+    }
+  
+    try {
+      await command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  });
 
 client.on('messageCreate', async function(msg) {
     if (msg.author.bot) {
